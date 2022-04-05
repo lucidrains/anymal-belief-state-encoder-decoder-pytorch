dd351f28aaa4de34a548c37d53cd49f27acbe629
Phil Wang <lucidrains@gmail.com>
2022-04-05 09:46:30 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'anymal-belief-state-encoder-decoder-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Anymal Belief-state Encoder Decoder - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/anymal-belief-state-encoder-decoder-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'attention gating',
+    'belief state',
+    'robotics'
+  ],
+  install_requires=[
+    'einops>=0.4',
+    'torch>=1.6',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
