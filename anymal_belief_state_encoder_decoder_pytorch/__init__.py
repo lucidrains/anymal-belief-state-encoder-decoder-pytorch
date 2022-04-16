1fc59f2ef3cd0aa026ac3348f1e59a8c694cd471
Phil Wang <lucidrains@gmail.com>
2022-04-16 11:00:04 -0700

    they used simple MLPs throughout the network, with leaky relu as activation

--- anymal_belief_state_encoder_decoder_pytorch/__init__.py
+++ anymal_belief_state_encoder_decoder_pytorch/__init__.py
@@ -1,1 +1,1 @@
-from anymal_belief_state_encoder_decoder_pytorch.networks import Student, Teacher
+from anymal_belief_state_encoder_decoder_pytorch.networks import Student, Teacher, MLP
--- anymal_belief_state_encoder_decoder_pytorch/networks.py
+++ anymal_belief_state_encoder_decoder_pytorch/networks.py
@@ -1,8 +1,45 @@
 import torch
 import torch.nn.functional as F
 from torch import nn, einsum
+from torch.nn import GRU
 
 from einops import rearrange
+
+# helper functions
+
+def exists(val):
+    return val is not None
+
+# add basic MLP
+
+class MLP(nn.Module):
+    def __init__(self, dims, activation = nn.LeakyReLU):
+        super().__init__()
+        assert isinstance(dims, (list, tuple))
+        dim_pairs = list(zip(dims[:-1], dims[1:]))
+        *dim_pairs, dim_out_pair = dim_pairs
+
+        layers = []
+        for dim_in, dim_out in dim_pairs:
+            layers.extend([
+                nn.Linear(dim_in, dim_out),
+                activation()
+            ])
+
+        layers.append(nn.Linear(*dim_out_pair))
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net(x)
+
+# they use basic PPO for the teacher
+
+class PPO(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x
 
 class Student(nn.Module):
     def __init__(self):
